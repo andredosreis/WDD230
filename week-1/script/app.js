6e75fd2887a83060e92cdb4961e8623a8919a7bd
andredosreis <63263157+andredosreis@users.noreply.github.com>
2023-01-15 00:37:02 +0000

    conclude first part exercise

--- week-1/script/app.js
+++ week-1/script/app.js
@@ -4,3 +4,5 @@ const h1 = document.querySelector('h1');
 
 h1.textContent = 'Andre R. silva';
 document.querySelector('h2').innerHTML =' WDD 230: Assaignment  Portal';
+let currentDate = document.lastModified;
+document.querySelector('#lastUpdate').innerHTML = 'Last Updated: ' + currentDate;
