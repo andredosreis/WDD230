a3b4a86fea61225de4d630cb6ab3479c9be9ead7
andredosreis <63263157+andredosreis@users.noreply.github.com>
2023-01-29 00:09:53 +0000

    modificado a data em JavaScript

--- week-1/script/app.js
+++ week-1/script/app.js
@@ -4,5 +4,4 @@ const h1 = document.querySelector('h1');
 
 h1.textContent = 'Andre R. Silva';
 document.querySelector('h2').innerHTML =' WDD 230: Assaignment  Portal';
-let currentDate = document.lastModified;
-document.querySelector('#lastUpdate').innerHTML = 'Last Updated: ' + currentDate;
+
